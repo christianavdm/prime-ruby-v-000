05df28ee32e6b70f80af4e4e92d51abecd7e6bcf
Christiana Vandermale <christianavdm@gmail.com>
2018-05-01 01:36:01 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,9 @@
 def prime?(num)
   array = (2...num).to_a
-  if array.any?{|x| num % x == 0}
+  if num < 1 
+    return false
+  elsif
+    array.any?{|x| num % x == 0}
     return false 
   else 
     return true
