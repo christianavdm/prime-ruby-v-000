656db758c5501b531fd479a219364213f41daeb0
Christiana Vandermale <christianavdm@gmail.com>
2018-05-01 01:33:57 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,8 @@
 def prime?(num)
   array = (2...num).to_a
-  array.any? do |x|
-    if num % x == 0
-    end
+  if array.any?{|x| num % x == 0}
+    return false 
+  else 
+    return true
   end
 end
